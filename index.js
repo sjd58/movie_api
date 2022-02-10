91718d0fa74cfae7d826eaf5b661164a2b7f1675
sjd58 <dicksonsj0798@gmail.com>
2022-02-10 13:21:31 -0500

    update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(myTopMovies);
 });
 
 //express.static to serve static files in public folder
@@ -29,14 +29,46 @@ app.listen(8080, () => {
   console.log('Your app is listening on port 8080, open your browser')
 })
 
-//Sample topMovie json object
-let topMovies = [
+//My Top Movies
+let myTopMovies = [
   {
-    title: 'topMovie1',
-    director: 'exampleDirector1'
+    title: 'The Godfather',
+    director: 'Francis Ford Coppola'
   },
   {
-    title: 'topMovie2',
-    director: 'exampleDirector2'
+    title: 'Once Upon a Time in... Hollywood',
+    director: 'Quentin Tarantino'
+  },
+  {
+    title: 'A Serious Man',
+    director: 'Joel Cohen'
+  },
+  {
+    title: 'The Shining',
+    director: 'Stanley Kubrick'
+  },
+  {
+    title: 'Tokyo Story',
+    director: 'Yasujiro Ozu'
+  },
+  {
+    title: 'The Enigma of Kaspar Hauser',
+    director: 'Werner Herzog'
+  },
+  {
+    title: 'The Seventh Seal',
+    director: 'Igmar Bergman'
+  },
+  {
+    title: 'The Long Goodbye',
+    director: 'Robert Altman'
+  },
+  {
+    title: 'The King of Comedy',
+    director: 'Martin Scorsesse'
+  },
+  {
+    title: 'Alien',
+    director: 'Ridley Scott'
   }
 ]
