e37ac280fc60b9b01ba42ad1750e7dde1d203186
sjd58 <dicksonsj0798@gmail.com>
2022-02-11 15:09:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,123 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
+//user and movie objects
+let movies = [
+  {
+    "Title": "The Godfather",
+    "Director": {
+      "Name": "Francis Ford Coppola"
+    },
+    "Genre": {
+      "Name": "Drama"
+    }
+  },
+  {
+    "Title": "Once Upon a Time in... Hollywood",
+    "Director": {
+      "Name": "Quentin Tarantino"
+    },
+    "Genre": {
+      "Name": "Comedy"
+    },
+  },
+  {
+    "Title": "A Serious Man",
+    "Director": {
+      "Name": "Joel Cohen"
+    },
+    "Genre": {
+      "Name": "Drama"
+    }
+  },
+  {
+    "Title": "The Shining",
+    "Director": {
+      "Name": "Stanley Kubrick"
+    },
+    "Genre": {
+      "Name": "Psychological Horror"
+    }
+  },
+  {
+    "Title": "Tokyo Story",
+    "Director": {
+      "Name": "Yasujiro Ozu",
+    },
+    "Genre": {
+      "Name": "Drama"
+    }
+  },
+  {
+    "Title": "The Enigma of Kaspar Hauser",
+    "Director": {
+      "Name": "Werner Herzog"
+    },
+    "Genre": {
+      "Name": "Historical Fiction"
+    }
+  },
+  {
+    "Title": "The Seventh Seal",
+    "Director": {
+      "Name": "Igmar Bergman"
+    },
+    "Genre": {
+      "Name": "Historical Fiction"
+    }
+  },
+  {
+    "Title": "The Long Goodbye",
+    "Director": {
+      "Name": "Robert Altman"
+    },
+    "Genre": {
+      "Name": "Thriller"
+    }
+  },
+  {
+    "Title": "The King of Comedy",
+    "Director": {
+      "Name": "Martin Scorsesse"
+    },
+    "Genre": {
+      "Name": "Comedy"
+    }
+  },
+  {
+    "Title": "Alien",
+    "Director": {
+      "Name": "Ridley Scott"
+    },
+    "Genre": {
+      "Name": "Horror"
+    }
+  }
+];
+
+let users = [
+  {
+    id: 1,
+    name: 'Jimmy',
+    favoriteMovies: []
+  },
+  {
+    id: 2,
+    name: 'Kimmy',
+    favoriteMovies: []
+  }
+];
+
+//after importing express needs to be added to the app
 const app = express();
 
 //morgan to log all requests
 app.use(morgan('common'));
+
+//use bodyParser to read data out of the request body
+app.use(bodyParser.json());
 
 //error handler
 app.use((err, req, res, next) => {
@@ -12,13 +125,122 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-// GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to my movie app!');
+// CREATE add user
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('users need names')
+  }
 });
 
+// UPDATE updatedUser.name
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user')
+  }
+});
+
+// CREATE add movieTitle to favoriteMovies
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user')
+  }
+});
+
+// DELETE remove favoriteMovies
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !==movieTitle);
+    res.status(200).send(`${movieTitle} has been removed to user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user')
+  }
+});
+
+// DELETE remove user
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id);
+
+  if (user) {
+    users = users.filter( user => user.id != id )
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send('no such user')
+  }
+});
+
+// READ 
 app.get('/movies', (req, res) => {
-  res.json(myTopMovies);
+  res.status(200).json(movies)
+});
+
+// READ movies by title
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.Title === title )
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('no such movie');
+  }
+});
+
+// READ, genre
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('no such genre')
+  }
+});
+
+// READ, director
+app.get('/movies/director/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.Director.Name === directorName ).Director
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director')
+  }
+});
+
+// READ 
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie app!');
 });
 
 //express.static to serve static files in public folder
@@ -28,47 +250,3 @@ app.use(express.static('public'));
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080, open your browser')
 })
-
-//My Top Movies
-let myTopMovies = [
-  {
-    title: 'The Godfather',
-    director: 'Francis Ford Coppola'
-  },
-  {
-    title: 'Once Upon a Time in... Hollywood',
-    director: 'Quentin Tarantino'
-  },
-  {
-    title: 'A Serious Man',
-    director: 'Joel Cohen'
-  },
-  {
-    title: 'The Shining',
-    director: 'Stanley Kubrick'
-  },
-  {
-    title: 'Tokyo Story',
-    director: 'Yasujiro Ozu'
-  },
-  {
-    title: 'The Enigma of Kaspar Hauser',
-    director: 'Werner Herzog'
-  },
-  {
-    title: 'The Seventh Seal',
-    director: 'Igmar Bergman'
-  },
-  {
-    title: 'The Long Goodbye',
-    director: 'Robert Altman'
-  },
-  {
-    title: 'The King of Comedy',
-    director: 'Martin Scorsesse'
-  },
-  {
-    title: 'Alien',
-    director: 'Ridley Scott'
-  }
-]
