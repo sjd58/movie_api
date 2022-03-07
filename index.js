6bce7f594f21f8272516f8a96bf93fb64f20639c
sjd58 <dicksonsj0798@gmail.com>
2022-03-07 11:23:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/myFlixdDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://sjd58:careerfoundry2022@myflixapi.xnkm2.mongodb.net/myFlixdDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb+srv://sjd58:careerfoundry2022@myflixapi.xnkm2.mongodb.net/myFlixdDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
   morgan = require('morgan'),
@@ -68,7 +68,7 @@ app.post('/users', [
   check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric character - not allowed').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
-  check('Email', 'Email does not appear to be valid').isEmail()
+  //check('Email', 'Email does not appear to be valid').isEmail() is this the problem?
 
 ], (req, res) => {
 
@@ -83,7 +83,7 @@ app.post('/users', [
   Users.findOne({ Username: req.body.Username }) //search to see if a user with the requested username already exists
     .then((user) => {
       if (user) { //If the user is found, send a response that it already exists
-        return res.status(400).send(req.body.Username + 'already exists');
+        return res.status(400).send(req.body.Username + ' already exists');
       } else {
         Users
           .create({
