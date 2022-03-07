08fc14c35dbeddceb1472996df0be16711a5fe62
sjd58 <dicksonsj0798@gmail.com>
2022-03-07 12:59:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/myFlixdDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://sjd58:careerfoundry2022@myflixapi.xnkm2.mongodb.net/myFlixdDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb+srv://sjd58:careerfoundry2022@myflixapi.xnkm2.mongodb.net/myFlixdDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
   morgan = require('morgan'),
