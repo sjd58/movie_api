67e702e945bc67ed3bde6e700417e792159c6b36
sjd58 <dicksonsj0798@gmail.com>
2022-03-15 11:13:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 });
 
 // READ movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', /*passport.authenticate('jwt', { session: false }),*/ (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
